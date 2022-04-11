9dcbdb233955cc49e429986cbf450fc382b37722
Rui Neto <ruineto11@gmail.com>
2022-04-11 00:11:01 -0300

    feat: GET /holidays/:month

--- index.js
+++ index.js
@@ -3,22 +3,32 @@ import holidays from "./holidays.js";
 
 const server = express();
 
-server.get("/holidays", (req, res) => {
+server.get("/holidays", (_req, res) => {
   res.send(holidays);
 });
 
-server.get("/is-today-holiday", (req, res) => {
+server.get("/is-today-holiday", (_req, res) => {
   const today = new Date();
 
   const todayIsHoliday = holidays.filter((holiday) => {
     return holiday === today.toLocaleDateString();
   });
 
-  if(todayIsHoliday.length !== 0){
+  if (todayIsHoliday.length !== 0) {
     res.send(`Sim, hoje é ${todayIsHoliday[0].name}`);
-  }else{
+  } else {
     res.send(`Não, hoje não é feriado`);
   }
+});
+
+server.get("/holidays/:month", (req, res) => {
+  const { month } = req.params;
+
+  const monthHolidays = holidays.filter((holiday) => {
+    return holiday.date.split("/")[0] === month.toString()
+  });
+
+  res.send(monthHolidays);
 });
 
 server.listen(3000);
